b7a4d8aa03f1d05e09dd97a623d54b39912b5ca2
arnobtarafder <rmtarnob@gmail.com>
2022-04-28 15:14:26 +0600

    add: upload product functionality

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
 require("dotenv").config();
@@ -9,6 +10,30 @@ const port = process.env.PORT || 5000
 app.use(cors());
 app.use(express.json())
 
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.n63qw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try{
+        await client.connect();
+        console.log("database connected");
+
+        const productCollection = client.db("gizmoFreak").collection("product")
+
+        app.post("/uploadProduct", async(req, res) => {
+          const product = req.body;
+          const result = await productCollection.insertOne(product);
+          res.send({success: "Product Uploaded Successfully"})
+        })
+    }
+    finally{
+
+    }
+}
+run().catch(console.dir)
 
 
 
